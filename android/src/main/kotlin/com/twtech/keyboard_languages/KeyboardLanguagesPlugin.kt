c717b09ba1a226b430bbfcc1e226733f79afc067
Yaniv Shaked <shakedyaniv@gmail.com>
2021-08-08 09:56:19 +0300

    [Android] Cleanup working version

--- android/src/main/kotlin/com/twtech/keyboard_languages/KeyboardLanguagesPlugin.kt
+++ android/src/main/kotlin/com/twtech/keyboard_languages/KeyboardLanguagesPlugin.kt
@@ -1,47 +1,24 @@
 package com.twtech.keyboard_languages
 
 import androidx.annotation.NonNull
-
-import android.app.Activity
-import android.content.Context
-import android.content.ContextWrapper
-import android.content.Intent
-import android.content.IntentFilter
-
 import android.os.LocaleList
-import android.os.Build.VERSION
-import android.os.Build.VERSION_CODES
-
-import android.view.inputmethod.InputMethodManager
-import android.view.inputmethod.InputMethodSubtype
-
-import io.flutter.embedding.engine.plugins.activity.ActivityAware
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-import io.flutter.plugin.common.PluginRegistry.Registrar
-
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
-import 	java.util.Locale
-
 /** KeyboardLanguagesPlugin */
-class KeyboardLanguagesPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
+class KeyboardLanguagesPlugin: FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
-  private lateinit var context: Context
-  private lateinit var activity: Activity
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "keyboard_languages")
     channel.setMethodCallHandler(this)
-    context = flutterPluginBinding.applicationContext
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
@@ -63,21 +40,5 @@ class KeyboardLanguagesPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
-  }
-
-  override fun onDetachedFromActivity() {
-    TODO("Not yet implemented")
-  }
-
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-      TODO("Not yet implemented")
-  }
-
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-      activity = binding.activity;
-  }
-
-  override fun onDetachedFromActivityForConfigChanges() {
-      TODO("Not yet implemented")
   }
 }
